70253486d74640b61770592f4dc1834e490ce1c2
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2023-04-19 09:39:55 -0700

    🚨 Format

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(clippy::all)]
 
-use actix_web::{web, App, HttpServer, HttpResponse};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
